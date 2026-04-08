c9412a4fff8f4b1d4b0863294182f07ddff72fe8
NimsaraWickramarathna@ <wenura2003@gmail.com>
2026-04-08 23:51:17 +0530

    extend frontend features

--- frontend/src/pages/resources/NewResourcePage.jsx
+++ frontend/src/pages/resources/NewResourcePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { createResource } from '../../api/resourceApi'
 import toast from 'react-hot-toast'
 
